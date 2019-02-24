3f384614831a1607cfc743c80633504d7ed5b5d9
Sean Chen <seanchen@Seans-MacBook-Pro.local>
2019-02-24 02:01:51 -0600

    add detail page specs

--- src/app/detail/detail.module.ts
+++ src/app/detail/detail.module.ts
@@ -4,9 +4,10 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { DetailRoutingModule } from './detail-routing.module';
 import { DetailComponent } from './detail.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, DetailRoutingModule],
+  imports: [CommonModule, TranslateModule, DetailRoutingModule, NgbModule],
   declarations: [DetailComponent]
 })
 export class DetailModule {}
